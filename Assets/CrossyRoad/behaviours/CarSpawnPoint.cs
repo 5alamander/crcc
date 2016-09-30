c77ee04162dbfb2bb83ff0b9c8b88a9a24cb4052
ZTGAME\baiyuyong <471073043@qq.com>
2016-09-30 09:50:22 +0800

    add car

---
+++ Assets/CrossyRoad/behaviours/CarSpawnPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace CrossyRoad {
+
+    public class CarSpawnPoint : NetworkBehaviour {
+
+        public GameObject Car;
+
+        void Start () {
+            
+        }
+
+        void Update () {
+
+            // spawn the car in server side
+            if (isServer) {
+                // spawn Car
+            }
+
+        }
+
+    }
+}
--- Assets/CrossyRoad/behaviours/Player.cs
+++ Assets/CrossyRoad/behaviours/Player.cs
@@ -8,11 +8,16 @@ namespace CrossyRoad {
     /// </summary>
     public class Player : NetworkBehaviour {
 
+        public string playerName;
+        public string playerColor;
+
         /// <summary>
         ///     set self name, to distinct from other player
         /// </summary>
         void Start () {
-            
+            if (isLocalPlayer) {
+                setSelfNameAndColor();
+            }
             // set self name, and self color
         }
 
@@ -20,6 +25,10 @@ namespace CrossyRoad {
             if (isLocalPlayer) {
                 // move the camera
             }
+        }
+
+        public void setSelfNameAndColor () {
+            
         }
     }
 }
