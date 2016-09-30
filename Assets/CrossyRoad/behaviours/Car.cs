a6cfdd0f77ec64c5f781670666b0caa5f9a33d05
ZTGAME\baiyuyong <471073043@qq.com>
2016-09-30 17:01:43 +0800

    format

--- Assets/CrossyRoad/behaviours/Car.cs
+++ Assets/CrossyRoad/behaviours/Car.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 using UnityEngine.Networking;
 
+/// <summary>
+///     spown car in server side,
+///     do not to change the speed
+/// </summary>
 public class Car : NetworkBehaviour {
 
 	// Use this for initialization
--- Assets/Scripts/LevelControllerScript.cs
+++ Assets/Scripts/LevelControllerScript.cs
@@ -1,26 +1,24 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class LevelControllerScript : MonoBehaviour {
-
     /// <summary>
-    /// Minimum line Z-position to be generated.
+    ///     Minimum line Z-position to be generated.
     /// </summary>
     public int minZ = 3;
 
     /// <summary>
-    /// Number of lines should be kept ahead.
+    ///     Number of lines should be kept ahead.
     /// </summary>
     public int lineAhead = 40;
 
     /// <summary>
-    /// Number of lines should be kept behind.
+    ///     Number of lines should be kept behind.
     /// </summary>
     public int lineBehind = 20;
 
     /// <summary>
-    /// Line prefabs for generation by controller.
+    ///     Line prefabs for generation by controller.
     /// </summary>
     public GameObject[] linePrefabs;
 
@@ -28,48 +26,49 @@ public class LevelControllerScript : MonoBehaviour {
 
     private GameObject player;
 
-    public void Start()
-    {
+    public void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
 
         lines = new Dictionary<int, GameObject>();
-	}
-	
-    public void Update()
-    {
+    }
+
+    public void Update () {
+        generateLines(player);
+    }
+
+    private void generateLines (GameObject thePlayer) {
         // Generate lines based on player position.
-        var playerZ = (int)player.transform.position.z;
-        for (var z = Mathf.Max(minZ, playerZ - lineBehind); z <= playerZ + lineAhead; z++)
-        {
-            if (!lines.ContainsKey(z))
-            {
-                var line = (GameObject)Instantiate(
-                    linePrefabs[Random.Range(0, linePrefabs.Length)],
-                    new Vector3(0, 0, z), Quaternion.identity
-                );
+        var playerZ = (int) thePlayer.transform.position.z;
+        for (var z = Mathf.Max(minZ, playerZ - lineBehind); z <= playerZ + lineAhead; z++) {
+            if (!lines.ContainsKey(z)) {
+                var line = getRandomLineAt(linePrefabs, z);
                 lines.Add(z, line);
             }
         }
 
         // Remove lines based on player position.
-        foreach (var line in new List<GameObject>(lines.Values))
-        {
+        foreach (var line in new List<GameObject>(lines.Values)) {
             var lineZ = line.transform.position.z;
-            if (lineZ < playerZ - lineBehind)
-            {
-                lines.Remove((int)lineZ);
+            if (lineZ < playerZ - lineBehind) {
+                lines.Remove((int) lineZ);
                 Destroy(line);
             }
         }
-	}
+    }
 
-    public void Reset()
-    {
+    /// <summary>
+    ///     get a random line at distance
+    /// </summary>
+    private static GameObject getRandomLineAt (IList<GameObject> prefabsArray, int distance) {
+        return (GameObject) Instantiate(
+            prefabsArray[Random.Range(0, prefabsArray.Count)],
+            new Vector3(0, 0, distance), Quaternion.identity);
+    }
+
+    public void Reset () {
         // TODO This kind of reset is dirty, refactor might be needed.
-        if (lines != null)
-        {
-            foreach (var line in new List<GameObject>(lines.Values))
-            {
+        if (lines != null) {
+            foreach (var line in new List<GameObject>(lines.Values)) {
                 Destroy(line);
             }
             Start();
