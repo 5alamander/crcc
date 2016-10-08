10d5ee78aedecfd336de9a8445a851cde04d8a89
byyMac <471073043@qq.com>
2016-10-08 15:53:43 +0800

    add basic pad

---
+++ Assets/BasicPanel/behaviours/BasicPad.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Sa1 {
+
+public class BasicPad : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler {
+    
+    public Vector2 direction {get; private set;}
+    public bool padTapped {get; private set;}
+    
+    public RectTransform padBottom;
+    public RectTransform padHandler;
+
+    public bool hasHandler = true;
+    
+    protected virtual void LateUpdate() {
+        padTapped = false;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        // Debug.Log("begin drag" + eventData.pressPosition);
+        if (!hasHandler) return;
+        padBottom.gameObject.SetActive(true);
+        padHandler.gameObject.SetActive(true);
+        // set position
+        padBottom.position = eventData.pressPosition;
+        padHandler.position = eventData.pressPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        direction = (eventData.position - eventData.pressPosition).normalized;
+        // Debug.Log("drag" + direction);
+        if (!hasHandler) return;
+        padHandler.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        // Debug.Log("end drag" + eventData.position);
+        direction = Vector2.zero;
+        if (!hasHandler) return;
+        padBottom.gameObject.SetActive(false);
+        padHandler.gameObject.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        padTapped = true;
+    }
+}
+
+}
--- Assets/BasicPanel/behaviours/BasicPanel.cs
+++ Assets/BasicPanel/behaviours/BasicPanel.cs
@@ -1,17 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BasicPanel : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler {
+namespace Sa1{
+    
+public class BasicPanel : BasicPad {
     private static BasicPanel _instance;
     public static BasicPanel Instance {
         get {return _instance;}
     }
-
-    public RectTransform padBottom;
-    public RectTransform padHandler;
-
-    public Vector2 direction {get; private set;}
-    public bool padTapped {get; private set;}
 
     public bool toggleButtonA {get; private set;}
     public bool toggleButtonB {get; private set;}
@@ -21,37 +17,11 @@ public class BasicPanel : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
         _instance = this;
     }
 
-    void LateUpdate() {
+    protected override void LateUpdate() {
+        base.LateUpdate();
         // refresh the toggle
         toggleButtonA = false;
         toggleButtonB = false;
-        padTapped = false;
-    }
-
-    public void OnBeginDrag(PointerEventData eventData) {
-        // Debug.Log("begin drag" + eventData.pressPosition);
-        padBottom.gameObject.SetActive(true);
-        padHandler.gameObject.SetActive(true);
-        // set position
-        padBottom.position = eventData.pressPosition;
-        padHandler.position = eventData.pressPosition;
-    }
-
-    public void OnDrag(PointerEventData eventData) {
-        direction = (eventData.position - eventData.pressPosition).normalized;
-        // Debug.Log("drag" + direction);
-        padHandler.position = eventData.position;
-    }
-
-    public void OnEndDrag(PointerEventData eventData) {
-        // Debug.Log("end drag" + eventData.position);
-        direction = Vector2.zero;
-        padBottom.gameObject.SetActive(false);
-        padHandler.gameObject.SetActive(false);
-    }
-
-    public void OnPointerClick(PointerEventData eventData) {
-        padTapped = true;
     }
 
     public void PressButtonA() {
@@ -61,4 +31,6 @@ public class BasicPanel : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     public void PressButtonB() {
         toggleButtonB = true;
     }
+}
+
 }
